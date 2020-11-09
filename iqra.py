9b468b30b259a84eb9bac99f83a52ea0eb66d241
iqra-sardar <74207834+iqra-sardar@users.noreply.github.com>
2020-11-10 00:21:29 +0500

    Update iqra.py

--- iqra.py
+++ iqra.py
@@ -66,10 +66,10 @@ banner = """
 \033[1;96m |_____\___\_\_|  \_\/_/    \_\
                                
 \033[1;91m-----------------------------------------------
-\033[1;92m➣ OWNER   : IQRA SARDAR
-\033[1;92m➣ Github  : https://github.com/iqra-sardar
-\033[1;92m➣ GANG    : BALOCH KILLER GANG
-\033[1;92m➣ Youtube : SUBSCRIBE CHANNEL
+\033[1;92m❤ OWNER   👉 IQRA SARDAR
+\033[1;92m❤ Github  👉 https://github.com/iqra-sardar
+\033[1;92m❤ GANG    👉 BALOCH KILLER GANG
+\033[1;92m❤ Youtube 👉 SUBSCRIBE CHANNEL
 \033[1;91m-----------------------------------------------"""
 # titik #
 def tik():
